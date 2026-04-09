a12a28667be859fe5c73beff0da5517f5bfee82f
ValeriaGonzalezE <valeriagoes170805@gmail.com>
2026-04-09 18:47:48 -0500

    Vista Crear Reserva
    Se modifico el espacio de nuevas reservas, para agregar un inicio y un fin (horario) para el espacio escogido

--- reserva-espacios/src/views/CreateReservation.vue
+++ reserva-espacios/src/views/CreateReservation.vue
@@ -3,23 +3,55 @@
     <div class="container">
       <h2>Crear Reserva</h2>
 
-      <input v-model="fecha" type="date" />
-      <input v-model="hora" type="time" />
+      <input type="date" v-model="fecha" @change="cargarDisponibles" />
 
-      <button @click="crear">Reservar</button>
+        <select v-model="espacioSeleccionado">
+          <option disabled value="">Seleccione espacio</option>
+          <option v-for="e in espacios" :key="e.id" :value="e.id">
+            {{ e.nombre }} - {{ e.tipo }}
+          </option>
+        </select>
+
+      <input type="time" v-model="horaInicio" />
+      <input type="time" v-model="horaFin" />
+
+      <button @click="reservar">Reservar</button>
+
     </div>
   </ion-page>
 </template>
 
 <script setup>
 import { IonPage } from '@ionic/vue'
-import { ref } from "vue";
+import { ref, onMounted } from "vue";
+import { useUserStore } from "@/stores/UserStore";
 
+import api from "@/services/api";
+
+const userStore = useUserStore();
+
+const espacios = ref([]);
+const espacioSeleccionado = ref("");
 const fecha = ref("");
-const hora = ref("");
+const horaInicio = ref("");
+const horaFin = ref("");
 
-const crear = () => {
-  alert(`Reserva creada: ${fecha.value} ${hora.value}`);
+const cargarDisponibles = async () => {
+  const res = await api.get(`/espacios-disponibilidad?fecha=${fecha.value}`);
+
+  espacios.value = res.data.filter(e => e.estado === "Disponible");
+};
+
+const reservar = async () => {
+  await api.post("/reservas", {
+    usuario_id: userStore.user.id,
+    espacio_id: espacioSeleccionado.value,
+    fecha: fecha.value,
+    hora_inicio: horaInicio.value,
+    hora_fin: horaFin.value
+  });
+
+  alert("Reserva creada");
 };
 </script>
 
