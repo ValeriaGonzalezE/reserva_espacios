64d610a6ae659f8a6c9c397ad1b51dfdc33d79fc
ValeriaGonzalezE <valeriagoes170805@gmail.com>
2026-05-01 19:26:20 -0500

    Reservación con restricciones y aviso
    Al asignar una fecha aparecen horarios ocupados del día
    
    Se realizo un bloqueo para no cruzar horarios de reservas

--- reserva-espacios/src/views/CreateReservation.vue
+++ reserva-espacios/src/views/CreateReservation.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, onMounted } from "vue";
+import { ref, onMounted, watch } from "vue";
 import { useRoute } from "vue-router";
 import { useUserStore } from "@/stores/UserStore";
 import api from "@/services/api";
@@ -8,6 +8,7 @@ import BackButton from "@/components/ui/BackButton.vue";
 import BaseInput from "@/components/ui/BaseInput.vue";
 import BaseButton from "@/components/ui/BaseButton.vue";
 import TimeBlocks from "@/components/reservations/TimeBlocks.vue";
+import PageHeader from "@/components/ui/PageHeader.vue";
 
 const userStore = useUserStore();
 const route = useRoute();
@@ -18,11 +19,28 @@ const horaInicio = ref("");
 const horaFin = ref("");
 const nombreEspacio = ref("");
 const horariosOcupados = ref([]);
+const usuario = ref({
+  nombre: "",
+  telefono: "",
+  correo: ""
+});
 
 onMounted(async () => {
   const res = await api.get("/espacios");
   const e = res.data.find(x => x.id == espacioSeleccionado.value);
   nombreEspacio.value = e?.nombre;
+
+  usuario.value = {
+    nombre: userStore.user.nombre,
+    telefono: userStore.user.telefono,
+    correo: userStore.user.correo
+  };
+});
+
+watch(fecha, (newVal) => {
+  if (newVal) {
+    cargarHorariosOcupados();
+  }
 });
 
 const cargarHorariosOcupados = async () => {
@@ -32,7 +50,32 @@ const cargarHorariosOcupados = async () => {
   horariosOcupados.value = res.data;
 };
 
+const hayCruce = () => {
+  const inicio = toMinutos(horaInicio.value);
+  const fin = toMinutos(horaFin.value);
+
+  return horariosOcupados.value.some(h => {
+    const hInicio = toMinutos(h.hora_inicio);
+    const hFin = toMinutos(h.hora_fin);
+
+    return inicio < hFin && fin > hInicio;
+  });
+};
+
 const reservar = async () => {
+
+  if (!fecha.value || !horaInicio.value || !horaFin.value) {
+    return alert("Completa todos los campos");
+  }
+
+  if (horaInicio.value >= horaFin.value) {
+    return alert("Hora fin debe ser mayor que hora inicio");
+  }
+
+  if (hayCruce()) {
+    return alert("Ese horario ya está ocupado");
+  }
+
   const res = await api.post("/reservas", {
     usuario_id: userStore.user.id,
     espacio_id: espacioSeleccionado.value,
@@ -42,31 +85,81 @@ const reservar = async () => {
   });
 
   if (!res.data.success) return alert(res.data.message);
+
   alert("Reserva creada");
+};
+
+const toMinutos = (hora) => {
+  const [h, m] = hora.split(":").map(Number);
+  return h * 60 + m;
 };
 </script>
 
 <template>
   <ion-page>
     <ion-content>
-      <div class="container">
 
-        <BackButton />
+      <div class="page">
 
-        <h2>Crear Reserva</h2>
+        <BackButton class="back-floating" />
+        <PageHeader titulo="CREAR RESERVA" />
 
-        <h3 v-if="nombreEspacio">Espacio: {{ nombreEspacio }}</h3>
+        <div class="container">
 
-        <BaseInput label="Fecha" type="date" v-model="fecha" @change="cargarHorariosOcupados" />
+          <h3 v-if="nombreEspacio">Espacio: {{ nombreEspacio }}</h3>
 
-        <TimeBlocks :horarios="horariosOcupados" />
+          <!-- INFO USUARIO -->
+          <div class="user-info">
+            <p><strong>Nombre:</strong> {{ usuario.nombre }}</p><br>
+            <p><strong>Celular:</strong> {{ usuario.telefono }}</p><br>
+            <p><strong>Correo:</strong> {{ usuario.correo }} </p>
+          </div>
 
-        <BaseInput label="Hora inicio" type="time" v-model="horaInicio" />
-        <BaseInput label="Hora fin" type="time" v-model="horaFin" />
+          <BaseInput label="Fecha" type="date" v-model="fecha" />
 
-        <BaseButton @click="reservar">Reservar</BaseButton>
+          <TimeBlocks :horarios="horariosOcupados" />
+
+          <BaseInput label="Hora inicio" type="time" v-model="horaInicio" />
+          <BaseInput label="Hora fin" type="time" v-model="horaFin" />
+
+          <BaseButton @click="reservar">Reservar</BaseButton>
+
+        </div>
 
       </div>
+
     </ion-content>
   </ion-page>
 </template>
+<style>
+.page {
+  min-height: 100vh;
+  padding: 20px;
+  background: linear-gradient(135deg, #0f0f0f, #1a0005);
+  color: white;
+}
+
+.container {
+  margin-top: 30px;
+  background: #1e1e1e;
+  padding: 25px;
+  border-radius: 15px;
+  box-shadow: 0 0 25px rgba(255, 46, 98, 0.6);
+}
+
+.user-info {
+  background: #2323233d;
+  padding: 15px;
+  border-radius: 10px;
+  margin-bottom: 15px;
+  font-size: 14px;
+  color: #e5e3e4f5
+}
+
+.back-floating {
+  position: absolute;
+  top: 32px;
+  left: 35px;
+  z-index: 10;
+}
+</style>
